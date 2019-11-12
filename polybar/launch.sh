d1bc4be5d84e6c8bdf3740f2b09ae8663b2ce673
fl0sh0d4n <florian.schneller@gmx.de>
2019-11-12 15:26:58 +0100

    Create launch.sh

---
+++ polybar/launch.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env sh
+
+# Terminate already running bar instances
+killall -q polybar
+
+# Wait until the processes have been shut down
+while pgrep -x polybar >/dev/null; do sleep 1; done
+
+#if type "xrandr"; then
+#  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+#    MONITOR=$m polybar --reload top &
+#  done
+#fi
+
+
+# Launch bar1 and bar2
+polybar  top &
+
+# Launch
+#polybar example &
+
+echo "Bar launched..."
